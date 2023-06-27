f5c60f0576fb34043e026f66e9364811a73e0253
Wilson08 <wilson.guayanay@epn.edu.ec>
2023-06-26 22:29:18 -0500

    Conf.

--- src/database.js
+++ src/database.js
@@ -2,6 +2,13 @@ const mongoose = require ('mongoose')
 
 const MONGODB_URI = 'mongodb://0.0.0.0:27017/portafolio'
 
+const {DBUSER,DBPASSWORD,DBNAME} = process.env
+
+//const MONGODB_URI = 'mongodb+srv://admin:admin@portafolio.lew06jt.mongodb.net/?retryWrites=true&w=majority'
+
+
+
+
 console.log(process.env.DBUSER)
 
 connection = async ()=>{
@@ -16,4 +23,4 @@ connection = async ()=>{
     }
 }
 
-module.exports = connection
+module.exports = connection 
